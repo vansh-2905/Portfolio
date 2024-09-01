62f20bda79dc51cec9cc3de352273a875ab146f6
Vansh Kapoor <134051375+vansh-2905@users.noreply.github.com>
2024-08-31 23:38:57 -0700

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -36,8 +36,8 @@ const Contact = () => {
             </div>
 
             <div className="bg-white/10 p-6 rounded-xl max-w-[550px]">
-                <h2 className="text-5xl font-bold text-orange-400 mb-4">Let's connect</h2>
-                <p className="text-white/70 mb-6">Send me a message and let's schedule a call</p>
+                <h2 className="text-5xl font-bold text-orange-400 mb-4">Lets connect</h2>
+                <p className="text-white/70 mb-6">Send me a message and lets schedule a call</p>
                 <form className="space-y-4" action="https://getform.io/f/brolwjwa" method="POST">
                     <div className="grid md:grid-cols-2 gap-4">
                         <input
