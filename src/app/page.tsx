e1179b087a90f425b9b245b77db799afec1ca08e
Vansh Kapoor <134051375+vansh-2905@users.noreply.github.com>
2024-11-10 00:54:23 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
       </Head>
         
-      <div className="">
+      <div className="bg-black">
         <Navbar/>
         <Hero/>
         <About/>
